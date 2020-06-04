f508875e8ab8a12059449c1ff00d055ef83aae06
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2020-06-04 22:43:14 +0800

    Starred bus arrival is now push instead of pull

---
+++ app/src/main/java/io/github/amanshuraikwar/nxtbuz/data/starred/di/StarredBusArrivalModule.kt
@@ -0,0 +1,15 @@
+package io.github.amanshuraikwar.nxtbuz.data.starred.di
+
+import dagger.Binds
+import dagger.Module
+import io.github.amanshuraikwar.nxtbuz.data.starred.delegate.BusArrivalsDelegate
+import io.github.amanshuraikwar.nxtbuz.data.starred.delegate.BusArrivalsDelegateImpl
+import javax.inject.Singleton
+
+@Module
+abstract class StarredBusArrivalModule {
+
+    @Binds
+    @Singleton
+    internal abstract fun a(a: BusArrivalsDelegateImpl): BusArrivalsDelegate
+}
--- app/src/main/java/io/github/amanshuraikwar/nxtbuz/ui/main/fragment/starred/StarredArrivalsViewModelDelegate.kt
+++ app/src/main/java/io/github/amanshuraikwar/nxtbuz/ui/main/fragment/starred/StarredArrivalsViewModelDelegate.kt
@@ -2,19 +2,27 @@ package io.github.amanshuraikwar.nxtbuz.ui.main.fragment.starred
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import io.github.amanshuraikwar.multiitemadapter.RecyclerViewListItem
 import io.github.amanshuraikwar.nxtbuz.data.CoroutinesDispatcherProvider
 import io.github.amanshuraikwar.nxtbuz.data.busarrival.model.Arrivals
+import io.github.amanshuraikwar.nxtbuz.data.busarrival.model.StarredBusArrival
 import io.github.amanshuraikwar.nxtbuz.data.busstop.model.BusStop
-import io.github.amanshuraikwar.nxtbuz.domain.busarrival.GetStarredBusStopsArrivalsUseCase
+import io.github.amanshuraikwar.nxtbuz.domain.starred.AttachStarredBusArrivalsUseCase
 import io.github.amanshuraikwar.nxtbuz.domain.busstop.GetBusStopUseCase
 import io.github.amanshuraikwar.nxtbuz.ui.list.*
+import io.github.amanshuraikwar.nxtbuz.ui.main.fragment.busstoparrivals.BusStopArrivalsViewModelDelegate
+import io.github.amanshuraikwar.nxtbuz.ui.main.fragment.model.Alert
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onCompletion
 import javax.inject.Inject
 import javax.inject.Named
 
+@ExperimentalCoroutinesApi
 class StarredArrivalsViewModelDelegate @Inject constructor(
-    private val getStarredBusStopsArrivalsUseCase: GetStarredBusStopsArrivalsUseCase,
+    private val attachStarredBusArrivalsUseCase: AttachStarredBusArrivalsUseCase,
     private val getBusStopUseCase: GetBusStopUseCase,
     @Named("starredListItems") private val _starredListItems: MutableLiveData<MutableList<RecyclerViewListItem>>,
     private val dispatcherProvider: CoroutinesDispatcherProvider
@@ -22,56 +30,43 @@ class StarredArrivalsViewModelDelegate @Inject constructor(
 
     private lateinit var viewModelScope: CoroutineScope
     private lateinit var onStarredItemClicked: (busStop: BusStop, busServiceNumber: String) -> Unit
-    private var arrivalsLoopJob: Job? = null
 
-    private val arrivalsLoopErrorHandler = CoroutineExceptionHandler { _, _ ->
-        Log.i(TAG, "arrivalsLoopErrorHandler: Exception thrown.")
-        startStarredBusArrivalsLoopDelayed()
-    }
-
+    @InternalCoroutinesApi
     fun start(
         coroutineScope: CoroutineScope,
         onStarredItemClicked: (busStop: BusStop, busServiceNumber: String) -> Unit
     ) = coroutineScope.launch(dispatcherProvider.io) {
         this@StarredArrivalsViewModelDelegate.onStarredItemClicked = onStarredItemClicked
-        viewModelScope = coroutineScope
-        arrivalsLoopJob?.cancel()
-        startStarredBusArrivalsLoop()
-    }
-
-    private fun startStarredBusArrivalsLoop() {
-        arrivalsLoopJob = viewModelScope.launch(arrivalsLoopErrorHandler) {
-            startArrivalsLoop()
-        }
-    }
-
-    private fun startStarredBusArrivalsLoopDelayed() {
-        arrivalsLoopJob = viewModelScope.launch(arrivalsLoopErrorHandler) {
-            startArrivalsLoop(REFRESH_DELAY)
-        }
-    }
-
-    private suspend fun startArrivalsLoop(initialDelay: Long = 0) =
-        withContext(dispatcherProvider.computation) {
-
-            delay(initialDelay)
-
-            while (isActive) {
-
-                _starredListItems.postValue(
-                    getStarredBusStopsArrivalsUseCase()
-                        .map {
-                            if (it.arrivals is Arrivals.Arriving)
-                                StarredBusArrivalItem(it, ::onStarredItemClicked)
-                            else
-                                StarredBusArrivalErrorItem(it)
-                        }
-                        .toMutableList()
-                )
-
-                delay(REFRESH_DELAY)
+        this@StarredArrivalsViewModelDelegate.viewModelScope = coroutineScope
+        attachStarredBusArrivalsUseCase("main-fragment-view-model")
+            .catch { throwable ->
+                FirebaseCrashlytics.getInstance().recordException(throwable)
             }
-        }
+            .onCompletion {
+                Log.i(TAG, "start: onCompletion")
+            }
+            .collect(
+                object : FlowCollector<List<StarredBusArrival>> {
+                    override suspend fun emit(value: List<StarredBusArrival>) {
+                        Log.d(TAG, "emit: ")
+                        handleStarredBusArrivalList(value)
+                    }
+                }
+            )
+    }
+
+    private fun handleStarredBusArrivalList(starredBusArrivalList: List<StarredBusArrival>) {
+        _starredListItems.postValue(
+            starredBusArrivalList
+                .map {
+                    if (it.arrivals is Arrivals.Arriving)
+                        StarredBusArrivalItem(it, ::onStarredItemClicked)
+                    else
+                        StarredBusArrivalErrorItem(it)
+                }
+                .toMutableList()
+        )
+    }
 
     private fun onStarredItemClicked(busStopCode: String, busServiceNumber: String) {
         viewModelScope.launch(dispatcherProvider.io) {
@@ -80,7 +75,6 @@ class StarredArrivalsViewModelDelegate @Inject constructor(
     }
 
     companion object {
-        private const val REFRESH_DELAY = 10000L
         private const val TAG = "StarredArrivalsViewMode"
     }
 }
