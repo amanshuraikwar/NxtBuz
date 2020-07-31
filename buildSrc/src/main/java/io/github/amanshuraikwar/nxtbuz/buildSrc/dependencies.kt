76a59980df2ced887fa81d18facd82400ba77b17
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2020-07-31 19:00:44 +0800

    Cleaned up gradle files by introducing buildSrc

---
+++ buildSrc/src/main/java/io/github/amanshuraikwar/nxtbuz/buildSrc/dependencies.kt
@@ -0,0 +1,112 @@
+package io.github.amanshuraikwar.nxtbuz.buildSrc
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:4.0.0"
+
+    const val threeTenAbp = "com.jakewharton.threetenabp:threetenabp:1.2.4"
+
+    const val junit = "junit:junit:4.13"
+
+    object Google {
+        const val material = "com.google.android.material:material:1.1.0"
+
+        const val analytics = "com.google.firebase:firebase-analytics:17.4.0"
+        const val crashlytics = "com.google.firebase:firebase-crashlytics:17.1.1"
+        const val crashlyticsGradle = "com.google.firebase:firebase-crashlytics-gradle:2.1.1"
+
+        const val gmsGoogleServices = "com.google.gms:google-services:4.3.3"
+
+        const val autoService = "com.google.auto.service:auto-service:1.0-rc4"
+
+        const val playServicesMap = "com.google.android.gms:play-services-maps:17.0.0"
+        const val playServicesLocation = "com.google.android.gms:play-services-location:17.0.0"
+    }
+
+    object Kotlin {
+        private const val version = "1.4.0-rc"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.3.8-1.4-rc-154"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+    }
+
+    object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.2.0-rc01"
+        const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0"
+
+        object Navigation {
+            private const val version = "2.2.2"
+            const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
+            const val ui = "androidx.navigation:navigation-ui-ktx:$version"
+        }
+
+        object Fragment {
+            private const val version = "1.2.3"
+            const val fragment = "androidx.fragment:fragment:$version"
+        }
+
+        object Test {
+
+            object Ext {
+                private const val version = "1.1.2-rc01"
+                const val junit = "androidx.test.ext:junit:$version"
+            }
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+
+        const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta6"
+
+        const val coreKtx = "androidx.core:core-ktx:1.3.0-rc01"
+
+        object Lifecycle {
+            private const val version = "2.2.0"
+            const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
+            const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
+            const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+
+        object Room {
+            private const val version = "2.2.5"
+            const val runtime = "androidx.room:room-runtime:$version"
+            const val compiler = "androidx.room:room-compiler:$version"
+            const val ktx = "androidx.room:room-ktx:$version"
+            const val testing = "androidx.room:room-testing:$version"
+        }
+    }
+
+    object Dagger {
+        private const val version = "2.28.3"
+        const val library = "com.google.dagger:dagger:$version"
+        const val androidSupport = "com.google.dagger:dagger-android-support:$version"
+        const val compiler = "com.google.dagger:dagger-compiler:$version"
+        const val androidProcessor = "com.google.dagger:dagger-android-processor:$version"
+    }
+
+    object Retrofit {
+        private const val version = "2.8.1"
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val gsonConverter = "com.squareup.retrofit2:converter-gson:$version"
+    }
+
+    object OkHttp {
+        private const val version = "4.7.2"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+    object KotlinPoet {
+        private const val version = "1.1.0"
+        const val library = "com.squareup:kotlinpoet:$version"
+    }
+
+    object Flipper {
+        const val library = "com.facebook.flipper:flipper:0.35.0"
+        const val soloader = "com.facebook.soloader:soloader:0.9.0"
+        const val networkPlugin = "com.facebook.flipper:flipper-network-plugin:0.35.0"
+    }
+}
