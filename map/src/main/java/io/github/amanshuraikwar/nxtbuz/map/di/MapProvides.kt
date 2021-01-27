a79d9f81cb8b230d422370f78860c72ac978f7dd
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2021-01-27 17:33:00 +0800

    bus stops screen

--- map/src/main/java/io/github/amanshuraikwar/nxtbuz/map/di/MapProvides.kt
+++ map/src/main/java/io/github/amanshuraikwar/nxtbuz/map/di/MapProvides.kt
@@ -1,10 +1,13 @@
 package io.github.amanshuraikwar.nxtbuz.map.di
 
+import com.google.android.gms.maps.model.Marker
 import dagger.Module
 import dagger.Provides
 import io.github.amanshuraikwar.nxtbuz.common.model.map.MapEvent
 import io.github.amanshuraikwar.nxtbuz.common.util.flow.ReturnableFlow
 import io.github.amanshuraikwar.nxtbuz.common.model.map.MapResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -17,5 +20,12 @@ class MapProvides {
     fun provideMapEventFlow(): ReturnableFlow<MapEvent, MapResult> {
         // TODO: 24/1/21 dynamically decide replay
         return ReturnableFlow(replay = 100)
+    }
+
+    @Provides
+    @Singleton
+    @Named("markerClicked")
+    fun provideMarkerClickedFlow(): MutableStateFlow<Marker?> {
+        return MutableStateFlow(null)
     }
 }
