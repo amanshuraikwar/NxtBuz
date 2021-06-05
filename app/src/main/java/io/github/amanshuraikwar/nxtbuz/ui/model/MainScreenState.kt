57adabff5a1381b1a55a2691f5f916d08b6043f6
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2021-06-06 02:06:00 +0800

    handled search back press

--- app/src/main/java/io/github/amanshuraikwar/nxtbuz/ui/model/MainScreenState.kt
+++ app/src/main/java/io/github/amanshuraikwar/nxtbuz/ui/model/MainScreenState.kt
@@ -2,15 +2,41 @@ package io.github.amanshuraikwar.nxtbuz.ui.model
 
 import io.github.amanshuraikwar.nxtbuz.common.model.BusStop
 
-sealed class MainScreenState {
-    object BusStops : MainScreenState()
+sealed class MainScreenState(
+    val searchVisible: Boolean
+) {
+    class BusStops(
+        searchVisible: Boolean
+    ) : MainScreenState(searchVisible)
 
-    data class BusStopArrivals(
+    class BusStopArrivals(
+        searchVisible: Boolean,
         val busStop: BusStop
-    ) : MainScreenState()
+    ) : MainScreenState(searchVisible)
 
-    data class BusRoute(
+    class BusRoute(
+        searchVisible: Boolean,
         val busStopCode: String,
         val busServiceNumber: String,
-    ) : MainScreenState()
+    ) : MainScreenState(searchVisible)
+
+}
+
+fun MainScreenState.BusRoute.copy(
+    searchVisible: Boolean
+): MainScreenState.BusRoute {
+    return MainScreenState.BusRoute(
+        searchVisible = searchVisible,
+        busServiceNumber = busServiceNumber,
+        busStopCode = busStopCode
+    )
+}
+
+fun MainScreenState.BusStopArrivals.copy(
+    searchVisible: Boolean
+): MainScreenState.BusStopArrivals {
+    return MainScreenState.BusStopArrivals(
+        searchVisible = searchVisible,
+        busStop = busStop,
+    )
 }
