83423870a770c457aa0303355f62f2dc7b82971d
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2021-06-06 19:10:55 +0800

    search screen animation

--- search/src/main/java/io/github/amanshuraikwar/nxtbuz/search/ui/SearchScreen.kt
+++ search/src/main/java/io/github/amanshuraikwar/nxtbuz/search/ui/SearchScreen.kt
@@ -1,12 +1,17 @@
 package io.github.amanshuraikwar.nxtbuz.search.ui
 
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.unit.dp
 import dev.chrisbanes.accompanist.insets.ExperimentalAnimatedInsets
 import io.github.amanshuraikwar.nxtbuz.common.model.BusStop
 import io.github.amanshuraikwar.nxtbuz.search.ui.model.SearchState
@@ -20,8 +25,28 @@ fun SearchScreen(
     onBusStopSelected: (BusStop) -> Unit = {},
     onBackClick: () -> Unit = {},
 ) {
+    var alpha by remember {
+        mutableStateOf(0f)
+    }
+    var offsetY by remember {
+        mutableStateOf(128.dp)
+    }
+
+    LaunchedEffect(null) {
+        animate(
+            initialValue = 0f,
+            targetValue = 1f,
+            animationSpec = tween(300)
+        ) { animatedValue, _ ->
+            alpha = animatedValue
+            offsetY = ((1 - animatedValue) * 128).dp
+        }
+    }
+
     Surface(
         modifier
+            .alpha(alpha = alpha)
+            .offset(y = offsetY)
             .fillMaxWidth()
             .fillMaxHeight()
     ) {
