db756623cc9e38ea45e733e29144532ddd02c49b
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2021-10-12 22:36:29 +0800

    ios client - cleanup

--- preferencestorage/src/iosMain/kotlin/io/github/amanshuraikwar/nxtbuz/preferencestorage/SettingsFactory.kt
+++ preferencestorage/src/iosMain/kotlin/io/github/amanshuraikwar/nxtbuz/preferencestorage/SettingsFactory.kt
@@ -7,10 +7,6 @@ actual class SettingsFactory(
     private val settingsSuiteName: String
 ) {
     actual fun createPreferenceStorage(): PreferenceStorage {
-        // TODO-amanshuraikwar (01 Oct 2021 09:08:45 PM):
-        //  this is not working
-        //  data is not being shared between app and widget
-
         return PreferenceStorageImpl {
             AppleSettings(NSUserDefaults(suiteName = settingsSuiteName))
         }
