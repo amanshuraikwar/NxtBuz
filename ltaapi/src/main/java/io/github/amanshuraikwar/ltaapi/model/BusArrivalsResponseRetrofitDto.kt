1cb49cdad49e542fc8a45b1d5a5ceb2cfd7e8eea
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2021-10-24 11:28:19 +0800

    retired lta api module

--- ltaapi/src/main/java/io/github/amanshuraikwar/ltaapi/model/BusArrivalsResponseRetrofitDto.kt
+++
@@ -1,9 +0,0 @@
-package io.github.amanshuraikwar.ltaapi.model
-
-import com.google.gson.annotations.SerializedName
-
-internal data class BusArrivalsResponseRetrofitDto(
-    @SerializedName("odata.metadata") val metadata: String,
-    @SerializedName("BusStopCode") val busStopCode: Int,
-    @SerializedName("Services") val busArrivals: List<BusArrivalItemRetrofitDto>
-)
