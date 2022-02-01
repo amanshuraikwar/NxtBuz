5c2e930c969d413721afbc6fd3c13e1b38a22fdf
Amanshu Raikwar <amanshuraikwar.in@gmail.com>
2022-02-01 15:47:36 +0800

    bus stop arrival item new ui

--- busstop-ui-android/src/main/java/io/github/amanshuraikwar/nxtbuz/busstop/arrivals/item/BusStopArrivalitem.kt
+++ busstop-ui-android/src/main/java/io/github/amanshuraikwar/nxtbuz/busstop/arrivals/item/BusStopArrivalitem.kt
@@ -1,20 +1,24 @@
 package io.github.amanshuraikwar.nxtbuz.busstop.arrivals.item
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowRight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.amanshuraikwar.nxtbuz.busstop.arrivals.model.BusStopArrivalListItemData
-import io.github.amanshuraikwar.nxtbuz.common.compose.StarIndicatorView
 import io.github.amanshuraikwar.nxtbuz.common.compose.SwipeableStarButtonView
+import io.github.amanshuraikwar.nxtbuz.commonkmm.arrival.BusLoad
+import io.github.amanshuraikwar.nxtbuz.commonkmm.arrival.BusType
 
 @ExperimentalAnimationApi
 @Composable
@@ -38,48 +42,68 @@ fun BusStopArrivalItem(
                     start = 16.dp,
                     bottom = 16.dp,
                     end = 16.dp
-                )
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
             when (data) {
                 is BusStopArrivalListItemData.BusStopArrival.Arriving -> {
-                    Box(
-                        contentAlignment = Alignment.BottomEnd
-                    ) {
+                    Column {
                         BusService(
-                            modifier
-                                .padding(
-                                    bottom = 4.dp,
-                                    end = 4.dp
-                                ),
                             busServiceNumber = data.busServiceNumber,
-                            busType = data.busType
+                            //busType = BusType.BD,
+                            starred = data.starred
                         )
 
-                        StarIndicatorView(
-                            isStarred = data.starred
-                        )
-                    }
-
-                    Column(
-                        modifier = Modifier.padding(top = 4.dp, start = 12.dp)
-                    ) {
-                        BusArrival(
-                            arrival = data.arrival,
-                            busLoad = data.busLoad,
-                            wheelchairAccess = data.wheelchairAccess
-                        )
-
-                        Spacer(modifier = Modifier.size(2.dp))
+                        Spacer(modifier = Modifier.size(4.dp))
 
                         BusDestination(
                             destinationBusStopDescription = data.destinationBusStopDescription
                         )
                     }
 
+                    Row(
+                        verticalAlignment = Alignment.Top
+                    ) {
+                        BusArrival(
+                            arrival = data.arrivingBusList.getOrNull(0)?.arrival ?: -1,
+                            busLoad = data.arrivingBusList.getOrNull(0)?.load ?: BusLoad.SEA,
+                            busType =
+                            data.arrivingBusList.getOrNull(0)?.type ?: BusType.SD
+                        )
+
+                        Icon(
+                            modifier = Modifier.padding(top = 8.dp),
+                            imageVector = Icons.Rounded.ArrowRight,
+                            contentDescription = "Next Bus Arrival"
+                        )
+
+                        BusArrival(
+                            arrival = data.arrivingBusList.getOrNull(1)?.arrival ?: -1,
+                            busLoad = data.arrivingBusList.getOrNull(1)?.load ?: BusLoad.SEA,
+                            busType =
+                            data.arrivingBusList.getOrNull(0)?.type ?: BusType.SD
+                        )
+
+                        Icon(
+                            modifier = Modifier.padding(top = 8.dp),
+                            imageVector = Icons.Rounded.ArrowRight,
+                            contentDescription = "Next Bus Arrival"
+                        )
+
+                        BusArrival(
+                            arrival = data.arrivingBusList.getOrNull(2)?.arrival ?: -1,
+                            busLoad = data.arrivingBusList.getOrNull(2)?.load ?: BusLoad.SEA,
+                            busType =
+                            data.arrivingBusList.getOrNull(0)?.type ?: BusType.SD
+                        )
+                    }
+
                 }
                 is BusStopArrivalListItemData.BusStopArrival.NotArriving -> {
-                    BusService(
+                    BusServiceDisabled(
                         busServiceNumber = data.busServiceNumber,
+                        starred = data.starred
                     )
 
                     Column(
